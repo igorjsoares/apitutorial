958956537e579633245e6a8f25d55a0846aae6aa
Igor Soares <igorjsoares@gmail.com>
2019-11-08 03:21:46 +0000

    Atualização teste Materialize - Conteudo

--- index.php
+++ index.php
@@ -21,6 +21,18 @@
             </div>
         </nav>
 
+        <!-- Page Layout here -->
+        <div class="row">
+
+            <div class="col s3">
+                <!-- Grey navigation panel -->
+            </div>
+
+            <div class="col s9">
+                <!-- Teal page content  -->
+            </div>
+
+        </div>
 
         <footer class="page-footer">
           <div class="container">
