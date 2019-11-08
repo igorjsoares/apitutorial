b34378b1293e71e85032cb2e27da56f7e3055027
Igor Soares <igorjsoares@gmail.com>
2019-11-08 02:59:55 +0000

    Atualização teste Materialize - Navbar

--- index.php
+++ index.php
@@ -12,7 +12,16 @@
     </head>
 
     <body>
-        <a class="waves-effect waves-light btn">button</a>
+        <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
 
 
         <footer class="page-footer">
