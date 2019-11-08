26ed4f2488bb503f800f62d33e9b5755e3af282a
Igor Soares <igorjsoares@gmail.com>
2019-11-08 02:47:08 +0000

    Atualização teste Materialize

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
-<?php
-echo "olá mundo";
+<!DOCTYPE html>
+  <html>
+    <head>
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+
+
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </body>
+  </html>
